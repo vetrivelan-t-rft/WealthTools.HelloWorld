63752de98df1aeab68f3d659647c68d3b5521698
vetrivelan <vetrivelan.t@thomsonreuters.com>
2019-09-25 10:42:42 +0530

    code added

---
+++ src/WealthTools.WebAPI.Logger/IUsageLogRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WealthTools.Common.Logger;
+using WealthTools.Common.Logger.Models;
+
+namespace WealthTools.WebAPI.Logger
+{
+    public interface IUsageLogRepository
+    {
+        string LogUsage(string usagedata);
+        List<UsageLogInfo> GetLastNLogs(int lastNLogs);
+    }
+}
